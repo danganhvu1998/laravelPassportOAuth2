34f06899f3462534d23cf002f3dc25737e8545bd
Dang Anh Vu <danganhvu1998@gmail.com>
2019-08-21 16:22:18 +0900

    add login end_point for OAuth2

---
+++ server/app/Http/Controllers/UsersController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+use Laravel\Passport\Http\Controllers\AccessTokenController;
+use Laravel\Passport\TokenRepository;
+use Lcobucci\JWT\Parser as JwtParser;
+use League\OAuth2\Server\AuthorizationServer;
+use Psr\Http\Message\ServerRequestInterface;
+
+class UsersController extends Controller
+{   
+    protected $server;
+    protected $tokens;
+    protected $jwt;
+
+
+    public function __construct(AuthorizationServer $server,
+                                TokenRepository $tokens,
+                                JwtParser $jwt)
+    {
+        $this->jwt = $jwt;
+        $this->server = $server;
+        $this->tokens = $tokens;
+    }
+
+    public function login(ServerRequestInterface $request)
+    {   # require username and password in $request
+        $controller = new AccessTokenController($this->server, $this->tokens, $this->jwt);
+
+        $request = $request->withParsedBody($request->getParsedBody() +
+        [
+            'grant_type' => 'password',
+            'client_id' => config('services.passport.client_id'),
+            'client_secret' => config('services.passport.client_secret'),
+        ]);
+
+
+        return with(new AccessTokenController($this->server, $this->tokens, $this->jwt))
+            ->issueToken($request);
+    }
+}
